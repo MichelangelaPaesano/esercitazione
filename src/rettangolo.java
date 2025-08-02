c6b00413bca825a8c5ba6d2cd334cf3292609651
MichelangelaPaesano <paesanomichelangela@gmail.com>
2025-08-02 14:11:07 +0200

    rettangolo

---
+++ src/rettangolo.java
@@ -0,0 +1,15 @@
+public class rettangolo {
+    public static void main(String[] args) {
+        //Rettangolo pieno di asterischi
+
+        for (int i = 0; i < 4; i++ ) {
+            for (int j = 0; j < 6; j++) {
+                System.err.print("*");
+            }
+
+            System.err.println("");
+        }
+
+
+}
+}
