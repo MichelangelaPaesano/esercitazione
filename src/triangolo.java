cc3722c96214086f7dcc7f4bd18b9f3d1a5ceb83
MichelangelaPaesano <paesanomichelangela@gmail.com>
2025-08-02 13:54:48 +0200

    albero

---
+++ src/triangolo.java
@@ -0,0 +1,15 @@
+public class triangolo {
+    
+
+    public static void main(String[] args) {
+        //triangolo di asterischi crescente
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j <= i; j++) {
+               System.out.print("*"); 
+            }
+            System.out.println("");
+        }
+        
+}
+}
